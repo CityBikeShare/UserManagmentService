1d9837859686fa798424ff2636f9cd5361c44774
Sebastjan Kozoglav <sk5429@student.uni-lj.si>
2018-11-24 17:22:31 +0100

    Login partially implemented

--- api/src/main/java/rest/sources/UsersLoginSource.java
+++ api/src/main/java/rest/sources/UsersLoginSource.java
@@ -5,49 +5,50 @@ import core.Users;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@Path("registerUser")
+@Path("loginUser")
 @ApplicationScoped
-public class UsersRegisterSource {
+public class UsersLoginSource {
     @Inject
     private UsersBean usersBean;
 
     @Operation(
-            description = "User registration",
+            description = "User login",
             tags = "users",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "User registration succeeded",
+                            description = "User login succeeded",
                             content = @Content(schema = @Schema(implementation = Users.class))
                     ),
                     @ApiResponse(
-                            responseCode = "400",
-                            description = "User registration failed",
-                            content = @Content(schema = @Schema(implementation = Error.class))
+                            responseCode = "401",
+                            description = "Username or password is not correct",
+                            content = @Content(schema = @Schema(implementation = Users.class))
                     )
             }
     )
-    @PUT
-    public Response registerUser(@RequestBody Users user) {
-        if (usersBean.getUserByUsername(user.getUsername()) != null) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
+    @POST
+    public Response loginUser(@QueryParam("uname") String username, @QueryParam("passwd") String password) {
+        Users user = usersBean.getUserByUsername(username);
+        if (user == null || !user.getPassword().equals(password)) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
-        user = usersBean.insertUser(user);
-        return Response.ok(user).build();
-    }
+        
+        return Response.ok().build();
 
+    }
 }
